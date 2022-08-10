1b24f5e51af227c1e768513403d2f68895f83bfc
Ricardo Svager Allet <91299767+ricardoallet@users.noreply.github.com>
2022-08-09 22:23:43 -0300

    conferencia_bool
    Analisa se o nome da cidade digitado tem uma palavra especifica. Validador simples de senha

---
+++ exe024.py
@@ -0,0 +1,6 @@
+cid = (input('Digite o nome de uma cidade: ')).strip()
+print(cid[:5].upper() == 'SANTO')
+
+cid = (input('Digite sua senha: ')).strip()
+x = (input('Repita sua senha: ')).strip()
+print(cid[:].upper() == x.upper())

a887545888335d773d6033c0d7b68786a7f0ad78
Ricardo Svager Allet <91299767+ricardoallet@users.noreply.github.com>
2022-08-23 22:49:08 -0300

    comparador
    Compara dois números

---
+++ exe038.py
@@ -0,0 +1,11 @@
+'''Code comparador numerico'''
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if num1 > num2:
+    print('O primeiro valor é maior')
+elif num2 > num1:
+    print('O segundo valor é maior')
+else:
+    print('Os dois valores são iguais')
